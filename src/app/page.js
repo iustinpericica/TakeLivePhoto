8326715f492fd3d525afcb7802799344539b5531
Iustin Pericica <ipericica@microsoft.com>
2023-05-11 18:22:38 +0300

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,94 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
 
-export default function Home() {
+import React,  { useState } from 'react';
+import Camera, { FACING_MODES, IMAGE_TYPES } from 'react-html5-camera-photo';
+import {ImagePreview} from './imagePreview';
+import 'react-html5-camera-photo/build/css/index.css';
+
+function App (props) {
+  const [dataUri, setDataUri] = useState('');
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  function handleTakePhoto (dataUri) {
+    // Do stuff with the photo...
+    console.log('takePhoto');
+  }
+
+  function handleTakePhotoAnimationDone (dataUri) {
+    // Do stuff with the photo...
+    console.log('takePhoto');
+    setDataUri(dataUri);
+  }
+
+  function handleCameraError (error) {
+    console.log('handleCameraError', error);
+  }
+
+  function handleCameraStart (stream) {
+    console.log('handleCameraStart');
+  }
+
+  function handleCameraStop () {
+    console.log('handleCameraStop');
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
+    <>
+    <div>
+      {
+        (dataUri)
+          ? <ImagePreview dataUri={dataUri}
+          />
+          :
+    <Camera
+      onTakePhoto = { (dataUri) => { handleTakePhoto(dataUri); } }
+      onTakePhotoAnimationDone = { (dataUri) => { handleTakePhotoAnimationDone(dataUri); } }
+      onCameraError = { (error) => { handleCameraError(error); } }
+      idealFacingMode = {FACING_MODES.ENVIRONMENT}
+      idealResolution = {{width: 640, height: 480}}
+      imageType = {IMAGE_TYPES.JPG}
+      imageCompression = {0.97}
+      isMaxResolution = {true}
+      isImageMirror = {false}
+      isSilentMode = {false}
+      isDisplayStartCameraError = {true}
+      isFullscreen = {false}
+      sizeFactor = {1}
+      onCameraStart = { (stream) => { handleCameraStart(stream); } }
+      onCameraStop = { () => { handleCameraStop(); } }
+    />
+      }
+    </div>
+    <div>
+      { dataUri && 
+        <button onClick={() => {setDataUri('')}}>Retake</button>
+      }
+    </div>
+    <div>
+    <hr class="dashed"></hr>
+    <label>Your Image File
+    <input
+        type="file"
+        name="myImage"
+        onChange={(event) => {
+          console.log(event.target.files[0]);
+          setSelectedImage(event.target.files[0]);
+        }}
+      />
+      { selectedImage && 
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          <img style={{width: "300px"}} src={URL.createObjectURL(selectedImage)} alt="myImage" />
+        </div>}
+    </label>
+    </div>
+    
+    { (!!selectedImage && !!dataUri) && 
+      <button>
+        Upload Images
+      </button>
+    }
+    </>
+  );
 }
+
+export default App;
